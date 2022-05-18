94be57e635decbd375292c109fd70cc5e806d86d
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-05-17 23:20:23 -0700

    i have the js methods working

--- index.js
+++ index.js
@@ -6,42 +6,43 @@ let colorCombo = []
 
 document.getElementById("chosen-color").addEventListener("input", function(e){
     e.preventDefault()
-
     color = this.value;
     cleanColor = color.slice(1)
 
-    console.log(cleanColor)
+    // console.log(cleanColor)
    
     
-    fetch(`https://www.thecolorapi.com/id?hex=${cleanColor}`)
-        .then(request => request.json())
-        .then(data => {
-            console.log(data)
+    // fetch(`https://www.thecolorapi.com/id?hex=${cleanColor}`)
+    //     .then(request => request.json())
+    //     .then(data => {
+    //         console.log(data)
             
 
 
             
-        //    document.getElementById("display-color").innerHTML = data.name.value
-        })
-
+    //     //    document.getElementById("display-color").innerHTML = data.name.value
+    //     })
 
     
 })
 
-document.getElementById("selector").addEventListener("input", function(e){
-    e.preventDefault()
-    schemeMode = e.target.value
-    fetch(`https://www.thecolorapi.com/scheme?hex=${cleanColor}&mode=${schemeMode}`)
-    .then(request => request.json())
-    .then(data => {
-        // let html = ""
-        for(let i = 0; i < data.colors.length; i++){
-                colorCombo.push(data.colors[i].hex.value)
-                console.log(colorCombo)
-        } 
-    })
+document.getElementById("selector").addEventListener("input", selectorFunction)
 
-})
+function selectorFunction(e){
+    e.preventDefault()
+    // colorCombo = []
+    schemeMode = e.target.value
+    // fetch(`https://www.thecolorapi.com/scheme?hex=${cleanColor}&mode=${schemeMode}`)
+    // .then(request => request.json())
+    // .then(data => {
+    //     // let html = ""
+    //     for(let i = 0; i < data.colors.length; i++){
+    //             colorCombo.push(data.colors[i].hex.value)
+    //             console.log(colorCombo)
+    //     } 
+    // })
+
+}
 
 
 document.getElementById("color-scheme").addEventListener("click", function(e){
@@ -49,6 +50,18 @@ document.getElementById("color-scheme").addEventListener("click", function(e){
     // fetch(`https://www.thecolorapi.com/scheme?hex=${cleanColor}&mode=${schemeMode}`)
     //     .then(request => request.json())
     //     .then(data => {
+
+        colorCombo = []
+    
+        fetch(`https://www.thecolorapi.com/scheme?hex=${cleanColor}&mode=${schemeMode}`)
+        .then(request => request.json())
+        .then(data => {
+            // let html = ""
+            for(let i = 0; i < data.colors.length; i++){
+                    colorCombo.push(data.colors[i].hex.value)
+                    console.log(colorCombo)
+            } 
+
             let html = ""
             for(let i = 0; i < colorCombo.length; i++){
                     html += `
@@ -58,8 +71,9 @@ document.getElementById("color-scheme").addEventListener("click", function(e){
                     `
             }
                 document.getElementById("display-color").innerHTML = html
+        })
+    
+        
 
-                colorCombo = []
-                console.log(colorCombo)
         
 })
